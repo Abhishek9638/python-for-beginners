a26de9e3d7740f170b25a675177d4c3b86b68652
Dhanani Abhishek <danani9638a@gmail.com>
2019-02-17 09:20:19 +0000

    tutorial 2

---
+++ Comment tutorial 2.py
@@ -0,0 +1,9 @@
+print("tutorial 2")
+
+print("comment symbol use   =>    #   ")
+
+print("For example  +>  ")
+
+#  comment +> symbol       #   
+#  comment sort cut   +>   ctrl + /
+#  uncomment sort cut   +>   ctrl + /
